10d43d440518d126c3dadd769aeff9a96a62a5ed
Andrea Tasca <andrea.t@heltyca.it>
2024-12-15 19:11:15 +0100

    Project setup -> Java porting

---
+++ src/main/java/io/atasc/intellij/nettunnel/NetTunnelPluginBundle.java
@@ -0,0 +1,26 @@
+package io.atasc.intellij.nettunnel;
+
+import com.intellij.DynamicBundle;
+import org.jetbrains.annotations.NonNls;
+import org.jetbrains.annotations.PropertyKey;
+
+public final class NetTunnelPluginBundle extends DynamicBundle {
+
+  @NonNls
+  private static final String BUNDLE = "messages.MyBundle";
+
+  private static final NetTunnelPluginBundle INSTANCE = new NetTunnelPluginBundle();
+
+  private NetTunnelPluginBundle() {
+    super(BUNDLE);
+  }
+
+  public static String message(@PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
+    return INSTANCE.getMessage(key, params);
+  }
+
+  @SuppressWarnings("unused")
+  public static String messagePointer(@PropertyKey(resourceBundle = BUNDLE) String key, Object... params) {
+    return INSTANCE.getLazyMessage(key, params).toString();
+  }
+}
