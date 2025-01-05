2813bf1830c97bbd5d1a455f8dfe3ce41d0323d7
Andrea Tasca <andrea.t@heltyca.it>
2025-01-05 18:03:55 +0100

    Icons

---
+++ src/main/java/io/atasc/intellij/tcptunnelj/ui/PluginIcons.java
@@ -0,0 +1,10 @@
+package io.atasc.intellij.tcptunnelj.ui;
+
+import com.intellij.openapi.util.IconLoader;
+
+import javax.swing.*;
+
+public interface PluginIcons {
+  Icon Action = IconLoader.getIcon("/icons/action.svg", PluginIcons.class);
+  Icon ToolWindow = IconLoader.getIcon("/icons/toolWindow.svg", PluginIcons.class);
+}
